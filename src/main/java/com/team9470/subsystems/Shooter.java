680fe937993a2e92d94d108c5adff362f9d5da2a
ZenoCoding <tychoyoung@gmail.com>
2024-06-21 15:06:53 -0700

    Shooter Notes

--- src/main/java/com/team9470/subsystems/Shooter.java
+++ src/main/java/com/team9470/subsystems/Shooter.java
@@ -3,5 +3,13 @@ package com.team9470.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Shooter
+ * <ul>
+ * <li>command to rpm</li>
+ * <li>conditional wait until rpm</li>
+ * </ul>
+ */
+
 public class Shooter extends SubsystemBase {
 }
--- src/main/java/com/team9470/subsystems/ShooterDeploy.java
+++ src/main/java/com/team9470/subsystems/ShooterDeploy.java
@@ -1,4 +1,8 @@
 package com.team9470.subsystems;
 
+/**
+ * Shooter Hood
+ *
+ */
 public class ShooterDeploy {
 }
